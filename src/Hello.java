f4b4ee4fa293cbd47a5b2fe921528098eb4ed217
Marlena Kowalska <marlena.kowalska.1990@gmail.com>
2019-06-11 19:22:35 +0200

    First Java Program

---
+++ src/Hello.java
@@ -0,0 +1,16 @@
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, My Dear Friend!");
+        int myFirstNumber = (15 - 3) * 10;
+        int mySecondNumber = 21;
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = (myFirstNumber + mySecondNumber + myThirdNumber);
+
+        int difference = 1000 - myTotal;
+        System.out.println(myTotal);
+        System.out.println(difference);
+    }
+}
